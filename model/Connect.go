2567a9b632671d0feed7f0f87d9b359b457c427a
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2023-07-05 17:04:25 +0700

    change database connection to mysql

--- model/Connect.go
+++ model/Connect.go
@@ -1,22 +1,22 @@
 package model
-
+ 
 import (
 	"fmt"
-
-	"gorm.io/driver/postgres"
+ 
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
-
+ 
 var DB *gorm.DB
-
+ 
 func ConnectDatabase() {
-	dsn := "user=IchsanR password=v7jfgMB6hEPW dbname=neondb host=ep-muddy-boat-134191.us-east-2.aws.neon.tech port=5432 sslmode=verify-full TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
+	dsn := "aprh8956_dbceria:dbceriangentot@tcp(103.247.8.36:3306)/aprh8956_dbceria?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
 		SingularTable: true,
 	}})
 	if err != nil {
-		fmt.Printf("Error")
+		fmt.Printf("Error: %v", err)
 		return
 	}
 	DB = db
